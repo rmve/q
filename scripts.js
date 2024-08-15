2727db09ddef6002355de5969213fb4c89e9233c
rmve <80171522+rmve@users.noreply.github.com>
2024-08-15 16:28:13 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('clickMe');
+    button.addEventListener('click', () => {
+        alert('Vous avez cliqué sur le bouton !');
+    });
+});
